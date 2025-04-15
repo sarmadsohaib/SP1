1ec6ad478e19763b7b187d1b204faa61a52f2e6f
Sam <ssohaib@gmail.com>
2025-04-15 18:59:03 +0300

    Version4

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
 this is SP2 that was clone from GitHub and now we are making first change
 
 I am making changes int it now and then i will push it to github as version3
+
+now this is clone of Version3 from GitHub. We have made few changes as if we are on another computer.
+NOw we want to see if these changes can be seen in the orign SP. Lets see. 
